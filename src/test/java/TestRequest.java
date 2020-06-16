29470b7c5b430ec1a42c6df6baba41e9e1008cdc
MakGlavocevic <pp_mak_glavocevic@hotmail.com>
2020-06-16 16:54:36 +0200

    latest update

--- src/test/java/TestRequest.java
+++ src/test/java/TestRequest.java
@@ -1,52 +1,67 @@
+
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Test;
-import org.junit.runner.Request;
+
 
 public class TestRequest {
+
     @Test
-    public void getListOfFlights()
-    {
+    public void getListOfFlights() {
+
         // base uri
-        RestAssured.baseURI = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/reference/v1.0/countries";
+        RestAssured.baseURI = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices";
 
-        // request object - initialisation
-        RequestSpecification httpRequest = RestAssured.given().header("x-rapidapi-key","a659416d69mshb0d9eaba28daa28p1ea282jsn96bf7025223c");
-
-
-        // resonse object
-        //Response response = httpRequest.request(Method.GET, "/fr-FR");
-//        Response res = RestAssured.given()
-//                .header("x-rapidapi-key","a659416d69mshb0d9eaba28daa28p1ea282jsn96bf7025223c")
-//                .when()
-//                .get ("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/reference/v1.0/countries/en-US").then().
-//                        extract().response();
-
-
-        // print response
         Response response;
-        String[] localeExamples = new String[] { "en-GB", "en-US", "fr-FR" };
+        String[] placeIds = new String[2];
+        String[] localeExamples = new String[] { "/autosuggest/v1.0/DE/EUR/en-GB/?query=Frankfurt", "/autosuggest/v1.0/DE/EUR/en-GB/?query=London"};
         for(int i=0; i<localeExamples.length; i++)
         {
+            System.out.println(localeExamples[i]);
+
+            RequestSpecification httpRequest = RestAssured.given().header("x-rapidapi-key","a659416d69mshb0d9eaba28daa28p1ea282jsn96bf7025223c");
+
             response = httpRequest.request(Method.GET, localeExamples[i]);
 
             if(response.getStatusCode() == 200)
             {
                 String responseBody = response.getBody().asString();
-                System.out.println("Request: " + httpRequest);
                 System.out.println("Response: " + responseBody);
+                JsonPath jsonPath = response.jsonPath();
+                String placeid = jsonPath.get("Places[0].PlaceId");
+                System.out.println(placeid);
+                placeIds[i] = placeid;
             }
 
             if(response.getStatusCode() == 400)
             {
-                response = httpRequest.request(Method.GET, "en-GB");
-                String responseBody = response.getBody().asString();
-                System.out.println("Response: " + responseBody);
-                //
+                System.out.println("You have placed Invalid Values, please try again.(BrowsePlaces)");
+
             }
         }
+        String getqoutes = "/browsedates/v1.0/DE/EUR/en-GB/"+ placeIds[0] +"/"+ placeIds[1] +"/2020-07-20/2020-07-29";
+        System.out.println(getqoutes);
 
+        RequestSpecification httpRequest = RestAssured.given().header("x-rapidapi-key","a659416d69mshb0d9eaba28daa28p1ea282jsn96bf7025223c");
+
+        response = httpRequest.request(Method.GET, getqoutes);
+
+        if(response.getStatusCode() == 200){
+
+        String responseBody = response.getBody().asString();
+        System.out.println("Response: " + responseBody);
+        JsonPath jsonPath = response.jsonPath();
+        Double MinPrice = jsonPath.getDouble("Quotes[0].MinPrice");
+        System.out.println(MinPrice);
+        }
+
+        if(response.getStatusCode() == 400)
+        {
+            System.out.println("You have placed Invalid Values, please try again.(BrowseDates)");
+
+        }
     }
 }
