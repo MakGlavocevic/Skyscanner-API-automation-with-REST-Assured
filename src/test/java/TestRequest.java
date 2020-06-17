e247d8467d88c7c44b162dfa644b92b79adfad36
MakGlavocevic <pp_mak_glavocevic@hotmail.com>
2020-06-17 15:34:51 +0200

    Update 1.0.3

--- src/test/java/TestRequest.java
+++ src/test/java/TestRequest.java
@@ -7,12 +7,11 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.Test;
 
 
-public class TestRequest {
+public class TestRequest{
     @Test
     public void getListOfFlights(){
         //Base Url
         RestAssured.baseURI = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices";
-
         Response response;
         String[] placeIds = new String[2];
         //Data for test "/autosuggest/v1.0/{Market}/{Currency}/{Locale}/?query={Place}"
@@ -21,12 +20,12 @@ public class TestRequest {
         {
             System.out.println(localeExamples[i]);
             //Valid rapidapi key
-            RequestSpecification httpRequest = RestAssured.given().header("x-rapidapi-key","a659416d69mshb0d9eaba28daa28p1ea282jsn96bf7025223c");
+            RequestSpecification httpRequest = RestAssured
+                    .given().header("x-rapidapi-key","a659416d69mshb0d9eaba28daa28p1ea282jsn96bf7025223c");
 
             response = httpRequest.request(Method.GET, localeExamples[i]);
 
-            if(response.getStatusCode() == 200)
-            {
+            if(response.getStatusCode() == 200){
                 String responseBody = response.getBody().asString();
                 System.out.println("Response: " + responseBody);
                 JsonPath jsonPath = response.jsonPath();
@@ -35,12 +34,10 @@ public class TestRequest {
                 placeIds[i] = placeid;
             }
 
-            if(response.getStatusCode() == 400)
-            {
+            if(response.getStatusCode() == 400) {
                 System.out.println("You have placed Invalid Values, please try again.(BrowsePlaces)");
             }
-            if(response.getStatusCode() == 500)
-            {
+            if(response.getStatusCode() == 500) {
                 System.out.println("Defect detected!");
             }
         }
@@ -52,17 +49,18 @@ public class TestRequest {
 
         response = httpRequest.request(Method.GET, getqoutes);
 
+        JsonPath jsonPath = response.jsonPath();
+        System.out.println(jsonPath.getString("Quotes"));
 
-        if(response.getStatusCode() == 200){
+        if(response.getStatusCode() == 200 && jsonPath.getString("Quotes") !=null && !jsonPath.getString("Quotes").equals("[]")){
         String responseBody = response.getBody().asString();
         System.out.println("Response: " + responseBody);
-        JsonPath jsonPath = response.jsonPath();
         Double MinPrice = jsonPath.getDouble("Quotes[0].MinPrice");
         String Carriers = jsonPath.getString("Carriers[0].Name");
         System.out.println("Price is : " + MinPrice);
         System.out.println("Carrier is : " + Carriers);
-        String Quotes = jsonPath.getString("Quotes");
-
+        } else {
+            System.out.println("There are no available routes!");
         }
 
         if(response.getStatusCode() == 400)
